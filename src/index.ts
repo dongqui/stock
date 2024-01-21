6250964520a142ecb489718d0e44000b59d4772e
Dongjin <dongqui9@gmail.com>
2024-01-21 19:21:48 +0900

    feat: KIS 연동, 매수 수량 조회 기능 추가

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
-import type { DART_DATA_TYPE } from "./types";
+import type { DART_DATA_TYPE, 최대_매수_수량_조회_응답 } from "./types";
+
+// https://apiportal.koreainvestment.com/apiservice/apiservice-domestic-stock#L_aade4c72-5fb7-418a-9ff2-254b4d5f0ceb
+
+const KIS_URL = "https://openapi.koreainvestment.com:9443";
+const token =
+  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6ImVmNDUyNGY1LTI2ZDctNDQwNS04YTlmLTNiODRjODI3MWJkYSIsImlzcyI6InVub2d3IiwiZXhwIjoxNzA1OTA3NzI3LCJpYXQiOjE3MDU4MjEzMjcsImp0aSI6IlBTYU9nYTQ5ekFLbzNuUWlZWDZoazNDMnJuOVhDcFB4REhqVyJ9.12RmYHC7MnLC5MZzsAZ-IpLfQ83_pPpGcJWQ0n40tWO3gHLH7GVrIclY7_OOzNtKV3xXHPNdxnyu6q6k1Hxdmg";
+const CANO = process.env.CANO;
+const ACNT_PRDT_CD = process.env.ACNT_PRDT_CD;
 
 async function pollingDataFromDart() {
   const results = await axios.get<DART_DATA_TYPE>(
@@ -14,3 +22,58 @@ async function pollingDataFromDart() {
 }
 
 pollingDataFromDart();
+
+async function orderStock(stockCode: string) {
+  const result = await axios.post(
+    `${KIS_URL}/uapi/domestic-stock/v1/trading/order-cash`,
+    {
+      CANO,
+      ACNT_PRDT_CD,
+      PDNO: stockCode,
+      ORD_QTY: "",
+      ORD_DVSN: "13",
+      ORD_UNPR: "0",
+    },
+    {
+      headers: {
+        authorization: token,
+        appkey: process.env.KIS_APP_KEY,
+        appsecret: process.env.KIS_APP_SECRET,
+        tr_id: "TTTC0802U",
+      },
+    }
+  );
+}
+
+async function getKISToken() {
+  const result = await axios.post(`${KIS_URL}/oauth2/tokenP`, {
+    grant_type: "client_credentials",
+    appkey: process.env.KIS_APP_KEY,
+    appsecret: process.env.KIS_APP_SECRET,
+  });
+  console.log(result);
+}
+
+async function 최대_매수_수량_조회(stockCode: string) {
+  const result = await axios.get<최대_매수_수량_조회_응답>(
+    `${KIS_URL}/uapi/domestic-stock/v1/trading/inquire-psbl-order`,
+    {
+      params: {
+        CANO,
+        ACNT_PRDT_CD,
+        PDNO: stockCode,
+        ORD_UNPR: "",
+        ORD_DVSN: "01",
+        CMA_EVLU_AMT_ICLD_YN: "N",
+        OVRS_ICLD_YN: "N",
+      },
+      headers: {
+        authorization: token,
+        appkey: process.env.KIS_APP_KEY,
+        appsecret: process.env.KIS_APP_SECRET,
+        tr_id: "TTTC8908R",
+      },
+    }
+  );
+  return result?.data?.output?.nrcvb_buy_qty;
+}
--- src/types.ts
+++ src/types.ts
@@ -17,3 +17,23 @@ export type DART_DATA_TYPE = {
     rm: string;
   }>;
 };
+
+export type 최대_매수_수량_조회_응답 = {
+  output: {
+    ord_psbl_cash: string;
+    ord_psbl_sbst: string;
+    ruse_psbl_amt: string;
+    fund_rpch_chgs: string;
+    psbl_qty_calc_unpr: string;
+    nrcvb_buy_amt: string;
+    nrcvb_buy_qty: string;
+    max_buy_amt: string;
+    max_buy_qty: string;
+    cma_evlu_amt: string;
+    ovrs_re_use_amt_wcrc: string;
+    ord_psbl_frcr_amt_wcrc: string;
+  };
+  rt_cd: string;
+  msg_cd: string;
+  msg1: string;
+};
